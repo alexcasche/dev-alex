6a1bde582753d5997b0014f497dc63285583bf05
Alex <alexcasche@gmail.com>
2018-09-25 08:44:53 -0400

    slate init

---
+++ slate.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+/*  Resources
+https://github.com/Shopify/slate/commit/8edd23a3ac6806c1b7b2645e29eb28ef7e2eff6f
+https://github.com/Shopify/slate/wiki/Slate-Configuration
+*/
+
+const postCSSNext = require('postcss-cssnext');
+const postCSSNested = require('postcss-nested');
+const postCSSImport = require('postcss-import');
+
+module.exports = {
+  'paths.theme.src': config => path.join(config.get('paths.theme'), 'src'),
+  'webpack.postcss.plugins': (config, defaultValue) => [
+    postCSSNext,
+    postCSSImport,
+    postCSSNested,
+    ...defaultValue
+  ]
+};
